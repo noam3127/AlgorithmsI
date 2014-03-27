08ebe6032fdeca5167771befc53b23a62a3104d2
Noam Lustiger <noshev27@gmail.com>
2014-03-27 16:10:52 -0400

    Final - 100% revised

--- PercolationProject/Percolation.java
+++ PercolationProject/Percolation.java
@@ -2,70 +2,87 @@
  *
  * @author Noam Lustiger
  */
-public class Percolation {
+public class Percolation  {
   
    private boolean[][] openSites;
-   private int N;
-   private int i;
-   private int j;
+   private int N;  
    private int single;
-   private int[] singleDim;
    private WeightedQuickUnionUF quick;
-   private int counter = 0;
-
-   public Percolation(int N){
+   private WeightedQuickUnionUF fullWQUF;
+   private int openCounter = 0;
+  
+   public Percolation(int N) {
        this.N = N;
-       singleDim = new int[(N + 1) * (N + 1)];      
        openSites = new boolean[N + 1][N + 1]; 
-       quick = new WeightedQuickUnionUF((N + 1) * (N + 1));    
-       
-       for(i = 0;i <= N;i++){
-           for(j = 0; j <= N;j++){                    
+       quick = new WeightedQuickUnionUF((N + 1) * (N + 1));  
+       fullWQUF = new WeightedQuickUnionUF((N + 1) * (N + 1));
+       for (int i = 0; i <= N; i++) {
+           for (int j = 0; j <= N; j++) {                    
                openSites[i][j] = false;            
              // System.out.println(i + ","+j+ " = " + xyTo1D(i,j));
            } 
        }       
        //connect all top sites "virtually"
-       for(i = N + 1; i < N * 2; i++){
+       for (int i = N + 1; i < N * 2; i++) {
            quick.union(i, i + 1);
+           fullWQUF.union(i, i + 1);
        }
        //connect all bottom sites "virtually"
-       for(i = N * N + N; i > (N * N) + 1 ; i--){
+       for (int i = N * N + N; i > (N * N) + 1; i--) {
            quick.union(i, i - 1);
-       }    
+       }  
    }
    
-   private int xyTo1D(int i, int j){
-       if(i == 0 || j == 0) return 0;
+   private int xyTo1D(int i, int j) {
+       if (i == 0 || j == 0) return 0;
        single = i * N + j;
        return single;
    }
    
-   public void open(int i, int j){
+   public void open(int i, int j) {
        if (i <= 0 || i > N) throw new IndexOutOfBoundsException("index i out of bounds");
        if (j <= 0 || j > N) throw new IndexOutOfBoundsException("index j out of bounds");             
-       openSites[i][j] = true;        
-       int singleD = xyTo1D(i,j);
-      // System.out.println(singleD);
-       if(j > 1 && openSites[i][j - 1])quick.union(singleD,xyTo1D(i,j - 1)); 
-       if(j < N - 1 && openSites[i][j + 1])quick.union(singleD,xyTo1D(i,j + 1)); 
-       if(i > 0 && openSites[i - 1][j])quick.union(singleD,xyTo1D(i - 1,j)); 
-       if(i < N - 1 && openSites[i + 1][j])quick.union(singleD,xyTo1D(i + 1,j));
+       openSites[i][j] = true;
+       openCounter++;
+       
+       int singleD = xyTo1D(i, j);
+       if (j > 1 && openSites[i][j - 1]) {
+           quick.union(singleD, xyTo1D(i, j - 1));
+           fullWQUF.union(singleD, xyTo1D(i, j - 1));
+       } 
+       if (j < N && openSites[i][j + 1]) {
+           quick.union(singleD, xyTo1D(i, j + 1));
+           fullWQUF.union(singleD, xyTo1D(i, j + 1));
+       } 
+       if (i > 0 && openSites[i - 1][j]) {
+           quick.union(singleD, xyTo1D(i - 1, j));
+           fullWQUF.union(singleD, xyTo1D(i - 1, j));
+       } 
+       if (i < N  && openSites[i + 1][j]) {
+           quick.union(singleD, xyTo1D(i + 1, j));
+           fullWQUF.union(singleD, xyTo1D(i + 1, j));
+       }   
    }
    
-   public boolean isOpen(int i, int j){      
+   public boolean isOpen(int i, int j) {  
+       if (i <= 0 || i > N) throw new IndexOutOfBoundsException("index i out of bounds");
+       if (j <= 0 || j > N) throw new IndexOutOfBoundsException("index j out of bounds");  
        return openSites[i][j];
    }
    
-   public boolean isFull(int i, int j){
-       int singleD = xyTo1D(i,j);
-       return quick.connected(singleD, 1);
+   public boolean isFull(int i, int j) {
+       if (i <= 0 || i > N) throw new IndexOutOfBoundsException("index i out of bounds");
+       if (j <= 0 || j > N) throw new IndexOutOfBoundsException("index j out of bounds");  
+       int singleD = xyTo1D(i, j);      
+       return openSites[i][j] && fullWQUF.connected(N + 1, singleD);
    }
    
-   public boolean percolates(){
-       return quick.connected(N + 1, (N * N) + N);    
+   public boolean percolates() {
+       if(N == 1 && openCounter == 1) return true; 
+       return quick.connected(N + 1, (N * N) + N) && openCounter > 1;   
+       
    }
-  // public static void main(String[] args){
-    //   new Percolation(20);       
-   //}  
+  /* public static void main(String[] args){
+       new Percolation(20);       
+   }  */
 }
--- PercolationProject/PercolationStats.java
+++ PercolationProject/PercolationStats.java
@@ -1,76 +1,77 @@
 
 /**
  *
- * @author noamlustiger
+ * @author Noam Lustiger
  * 
  
  */
 public class PercolationStats {
     private double mean;
     private int T;
-    private int N;
-    private int[]thresholds;
-    private double[]threshPercent;
-    private double stddev;
-    private double confidenceLo;
-    private double confidenceHi;
+    private double[]threshPercent;   
     private int thresholdCounter;
-    double percent;
-    Percolation perc;
     
-    public PercolationStats(int N, int T){
-        if(N <= 0 || T <= 0) throw new IllegalArgumentException("Both numbers must be greater than 0."); 
+    public PercolationStats(int N, int T) {
+        if (N <= 0 || T <= 0) throw new IllegalArgumentException("Both numbers must be greater than 0."); 
         this.T = T;
-        this.N = N;
-        thresholds = new int[T];
         threshPercent = new double[T];
-        for(int i = 0;i < T;i++){ 
-            perc = new Percolation(N); 
+        
+        for (int i = 0; i < T; i++) { 
+            Percolation perc = new Percolation(N); 
             thresholdCounter = 0;
             int randI;
             int randJ; 
-            while(!perc.percolates()){
+           // System.out.println(perc.percolates());
+            while (!perc.percolates()) {
                randI = StdRandom.uniform(N) + 1;
                randJ = StdRandom.uniform(N) + 1;
-               if(!perc.isOpen(randI,randJ)){
+               if (!perc.isOpen(randI, randJ)) {
                   perc.open(randI, randJ);
                   thresholdCounter++;
-               }                
-            }            
-            thresholds[i] = thresholdCounter;
+               } 
+               //This can be used as a visualizer from the command-line
+             /* 
+               if(perc.percolates()){
+                   
+                for(int j = 1; j <= N; j++){
+                    for(int n = 1; n <= N; n++){
+                        char c;
+                        if(perc.isFull(j, n)) c = 'O';
+                        else if(perc.isOpen(j, n)) c = ' ';
+                        else c = 'X';
+                      System.out.print("[" + c + "]");
+                     
+                    }
+                    System.out.println();
+                }
+                System.out.println("\n\n");               
+            }*/      
             threshPercent[i] = (double) thresholdCounter / (N * N);           
-        }        
-        System.out.println("MEAN: " + mean());
+            }  
+        }
+       /* System.out.println("MEAN: " + mean());
         System.out.println("high: " + confidenceHi());
-        System.out.println("low: " + confidenceLo());
+        System.out.println("low: " + confidenceLo()); */
     }
-    public double mean(){
+    public double mean() {
         mean = StdStats.mean(threshPercent);
         return mean;
     }    
-    public double stddev(){
+    public double stddev() {
         return StdStats.stddev(threshPercent);
     }
-    public double confidenceLo(){
-        confidenceLo = mean() - ((1.96 * stddev() / Math.sqrt(T)));
+    public double confidenceLo() {
+        double confidenceLo = mean() - ((1.96 * stddev() / Math.sqrt(T)));
         return confidenceLo;
     }
-    public double confidenceHi(){
-        confidenceHi = mean() + ((1.96 * stddev() / Math.sqrt(T)));
+    public double confidenceHi() {
+        double confidenceHi = mean() + ((1.96 * stddev() / Math.sqrt(T)));
         return confidenceHi;
     }
     
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int N = Integer.parseInt(args[0]);
         int T = Integer.parseInt(args[1]);
         new PercolationStats(N, T);
     }
 }
-/* public class PercolationStats {
-   public PercolationStats(int N, int T)    // perform T independent computational experiments on an N-by-N grid
-   public double mean()                     // sample mean of percolation threshold
-   public double stddev()                   // sample standard deviation of percolation threshold
-   public double confidenceLo()             // returns lower bound of the 95% confidence interval
-   public double confidenceHi()             // returns upper bound of the 95% confidence interval
-   public static void main(String[] args)   // test client, described below
-}*/
